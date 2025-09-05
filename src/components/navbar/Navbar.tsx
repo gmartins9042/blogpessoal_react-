3004693820df63fb3c7c206517c6b80568458364
gmartins9042 <gmartins9042@gmail.com>
2025-09-04 22:21:28 -0300

    feat: Crud Postagem

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,9 +22,10 @@ function Navbar() {
         <div className="container flex justify-between text-lg mx-8">
           <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
           <div className="flex gap-4">
-            Postagem
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
             <Link to='/temas' className='hover:underline'>Temas</Link>
-            <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>            Perfil
+            <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+            Perfil
             <Link to='' onClick={logout} className='houver:underline'>Sair</Link>
           </div>
         </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,6 @@
+import ListaPostagens from "../../components/postagem/listapostagens/listarPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
+
 function Home() {
     return (
         <>
@@ -11,9 +14,9 @@ function Home() {
                             Expresse aqui seus pensamentos e opniões
                         </p>
                         <div className="flex justify-around gap-4">
-                            <div className="rounded text-white border-white border-solid border-2 py-2 px-4">
-                                Nova Postagem
-                            </div>
+                            
+                             <ModalPostagem />
+                             
                         </div>
                     </div>
                     <div className="flex justify-center ">
@@ -25,6 +28,7 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <ListaPostagens/>
         </>
     )
 }
